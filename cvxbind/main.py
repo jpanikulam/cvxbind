dffd1ead5650e5fc4e6bf07c72ea7a10ed180e4c
Jacob Panikulam <jpanikulam@ufl.edu>
2015-08-28 00:14:31 -0400

    Add working main

---
+++ cvxbind/main.py
@@ -0,0 +1,20 @@
+import os
+import argparse
+from utils import Log
+from parse_cvxgen import ParseCVX
+from gen_cpp import GenCPP
+import argparse
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='CVXGEN Python Binding Generator')
+    parser.add_argument('path', metavar='path', default='./images',
+                        help='Give the target path')
+    parser.add_argument('-v', '--verbose', action='store_true', default=False,
+                        help='Decide verbosity')
+    args = parser.parse_args()
+    Log.set_verbose(args.verbose)
+    path = os.path.realpath(args.path)
+    parsed_cvx = ParseCVX.read_file(path)
+    write_text = GenCPP.make_cvx_binding(parsed_cvx)
+    print write_text
