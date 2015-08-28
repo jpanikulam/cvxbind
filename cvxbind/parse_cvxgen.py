030d790a0cb36544041d13a01f075f6f3c93aa9e
Jacob Panikulam <jpanikulam@ufl.edu>
2015-08-28 00:10:20 -0400

    CVX: Clean up parser

--- cvxbind/parse_cvxgen.py
+++ cvxbind/parse_cvxgen.py
@@ -49,8 +49,11 @@ class ParseCVX(object):
                 else:
                     section = line
 
-            elif section in section_dict.keys():
-                    content[section].append(section_dict[section](line))
+            elif section == 'dimensions':
+                content[section].append(self.parse_dimension(line))
+
+            elif section in ['parameters', 'variables']:
+                content[section].append(self.parse_parameter(line))
 
             else:
                 Log.warn("Unknown line {}: {}".format(l_num, line))
@@ -61,19 +64,30 @@ class ParseCVX(object):
     def parse_parameter(self, line):
         '''
             dimension_expr: string expression for reflecting dimension
-
-        Should have used regex!
+        TODO:  
+            - Should have used regex!
+            - Don't do blind text-replacement for special structure flags
         '''
         split = Utils.clean_split(line, None, maxsplit=1)
+        if len(split) == 1:
+            name = split[0]
+            _type = 'scalar'
+            return {
+                'name': name,
+                'dimension_expr': None,
+                'special': None,
+                'array_bounds': None,
+                'type': _type
+            }
 
         name, second_half = split[:2]
         Log.log("{}: Found parameter or variable".format(name))
 
         # Handle special structure flags
-        special_features = ['psd', 'diag', 'nsd', 'diagonal']
+        special_features = ['psd', 'nsd', 'diagonal']
         special = []
         for special_feature in special_features:
-            if second_half.endswith(special_feature):
+            if special_feature in second_half:
                 special.append(special_feature)
                 Log.log("{}: Registering special behavior: {}".format(name, special))
                 second_half = second_half.replace(special_feature, '')
@@ -82,14 +96,15 @@ class ParseCVX(object):
         if '[' in name:
             name = name.split('[')[0]
             array_expr = second_half.split(', ')[-1]
-            second_half = second_half.split(array_expr)[0]
+            second_half = second_half.split(', ' + array_expr)[0]
 
-            array_expr = second_half.split('=')[-1]
+            array_expr = array_expr.split('=')[-1]
             lower_bound, upper_bound = array_expr.split('..')
             array_bounds = (lower_bound, upper_bound)  # Strings
+            Log.log("{}: Registering array bounds expression: [{}: {}]".format(name, lower_bound, upper_bound))
 
         else:
-            array_length = None
+            array_bounds = None
 
         dimensions = second_half
 
@@ -97,17 +112,20 @@ class ParseCVX(object):
         dimensions = Utils.remove_chars(dimensions, '(', ')')
         if ',' in dimensions:
             dimension_expr = dimensions.replace(',', ' *')
+            _type = 'matrix'
 
         else:
+            _type = 'vector'
             dimension_expr = dimensions
 
-        Log.log("{}: Registering dimension expression: {}".format(name, dimension_expr))
+        Log.log("{}: Registering vector dimension expression: {}".format(name, dimension_expr))
 
         parameter = {
             'name': name,
             'dimension_expr': dimension_expr,
             'special': special,
-            'array_length': array_length,
+            'array_bounds': array_bounds,
+            'type': _type
         }
         return parameter
 
@@ -116,6 +134,7 @@ class ParseCVX(object):
         dim_name, dim_value = line.split('=')
         dim_value = int(dim_value)  # Explicitly convert to int
 
+        Log.log("{}: Registering dimension: {}".format(dim_name, dim_value))
         dimension = {
             'name': dim_name,
             'value': dim_value
